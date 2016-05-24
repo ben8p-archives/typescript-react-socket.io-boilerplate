55abb0b1db81c0364a42e5df569ee3ed14ba2d1f
Benjamin Santalucia <ben@dojotoolkit-fr.org>
2016-05-24 13:16:53 +0200

    add promise

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,11 +1,38 @@
 import * as React from "react";
+import * as promise from "promise";
 
 import "./styles/Hello.less";
 
 export interface HelloProps { name: string; lastname: string; }
 
+interface HelloState {
+    question: string;
+}
+
 export class Hello extends React.Component<HelloProps, {}> {
+    state: HelloState;
+
+    constructor() {
+      super();
+      this.state = this.getInitialState();
+    }
+
+     getInitialState():HelloState {
+        return {question: ""};
+    }
+
+    componentDidMount() {
+      let promise: Promise.IThenable<Boolean> = new Promise(function(resolve: (value: Boolean) => void, reject: (reason: Boolean) => void) {
+          setTimeout(function() {
+            resolve(true);
+          }, 2500);
+      });
+      promise.then((value: Boolean) => {
+          this.setState({question: "And you ?"});
+      });
+    }
+
     render() {
-        return <h1>Hello I am {this.props.name}, {this.props.lastname}!</h1>;
+        return <h1>Hello I am {this.props.name} {this.props.lastname}! {this.state.question}</h1>;
     }
 }
